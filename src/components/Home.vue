e31c7579de10a4b3a4f7cd906331bc8ef9efddb0
未通过二维图 <3341425593@qq.com>
2022-10-25 04:06:38 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <di>
+    <el-button type="info" @click="logout">退出</el-button>
+  </di>
+</template>
+
+<script>
+export default {
+  name: 'MyHome',
+  methods: {
+    logout() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
