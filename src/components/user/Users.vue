1672a886065a2364c777d8450dbea07a80768cd5
未通过二维图 <3341425593@qq.com>
2022-10-28 07:42:37 +0800

    完成了权限功能的开发

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -75,6 +75,7 @@
                 type="warning"
                 icon="el-icon-setting"
                 size="mini"
+                @click="setRole(scope.row)"
               ></el-button>
             </el-tooltip>
           </template>
@@ -156,6 +157,36 @@
         <el-button type="primary" @click="editUserInfo">确 定</el-button>
       </span>
     </el-dialog>
+
+    <!-- 分配角色对话框 -->
+    <el-dialog
+      title="分配角色"
+      :visible.sync="setRoleDialogVisible"
+      width="50%"
+      @close="setRoleDialogClosed"
+    >
+      <div>
+        <p>当前的用户：{{ userInfo.username }}</p>
+        <p>当前的角色：{{ userInfo.role_name }}</p>
+        <p>
+          分配新角色：
+          <el-select v-model="selectedRoleId" placeholder="请选择">
+            <el-option
+              v-for="item in rolesList"
+              :key="item.id"
+              :label="item.roleName"
+              :value="item.id"
+            >
+            </el-option>
+          </el-select>
+        </p>
+        <!-- {{ userInfo }} -->
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="setRoleDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="saveRoleInfo">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -164,7 +195,7 @@ export default {
   name: 'MyUser',
   data() {
     // 验证邮箱的规则
-    var checkEmail = (rule, value, cb) => {
+    const checkEmail = (rule, value, cb) => {
       const regEmail = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/
 
       if (regEmail.test(value)) {
@@ -175,7 +206,7 @@ export default {
     }
 
     // 验证手机号的规则
-    var checkMobile = (rule, value, cb) => {
+    const checkMobile = (rule, value, cb) => {
       const regMobile =
         /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/
 
@@ -238,7 +269,12 @@ export default {
       mobile: [
         { required: true, message: '请输入用户手机', trigger: 'blur' },
         { validator: checkMobile, trigger: 'blur' }
-      ]
+      ],
+      setRoleDialogVisible: false,
+      userInfo: {},
+      rolesList: [],
+      selectedRoleId: ''
+      // 数据数据数据数据数据数据数据数据数据数据数据数据数据数据数据数
     }
   },
   created() {
@@ -254,7 +290,7 @@ export default {
       }
       this.userlist = res.data.users
       this.total = res.data.total
-      console.log(res)
+      // console.log(res)
     },
 
     handleSizeChange(newSize) {
@@ -352,7 +388,41 @@ export default {
       }
       this.$message.success('删除用户成功！')
       this.getUserList()
+    },
+    async setRole(userIn) {
+      this.userInfo = userIn
+      console.log(this.userInfo)
+      const { data: res } = await this.$http.get('roles')
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取角色列表失败！')
+      }
+      this.rolesList = res.data
+
+      this.setRoleDialogVisible = true
+    },
+    async saveRoleInfo() {
+      if (!this.selectedRoleId) {
+        return this.$message.error('请选择要分配的角色！')
+      }
+
+      const { data: res } = await this.$http.put(
+        `users/${this.userInfo.id}/role`,
+        {
+          rid: this.selectedRoleId
+        }
+      )
+      if (res.meta.status !== 200) {
+        return this.$message.error('更新角色失败！')
+      }
+      this.$message.success('更新角色成功！')
+      this.getUserList()
+      this.setRoleDialogVisible = false
+    },
+    setRoleDialogClosed() {
+      this.selectedRoleId = ''
+      this.userInfo = {}
     }
+    // 方法方法方法方法方法方法方法方法方法方法方法方法方法方法
   }
 }
 </script>
