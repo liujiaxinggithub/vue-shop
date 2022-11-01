a442a770665ae643a7bc0afbb097e49e7e7c2306
未通过二维图 <3341425593@qq.com>
2022-11-01 09:25:51 +0800

    完成商品功能开发

--- src/main.js
+++ src/main.js
@@ -9,6 +9,10 @@ import './assets/css/global.css'
 
 import axios from 'axios'
 import TreeTable from 'vue-table-with-tree-grid'
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 
 Vue.prototype.$http = axios
 // 配置请求的根路径
@@ -20,6 +24,21 @@ axios.interceptors.request.use((config) => {
 Vue.config.productionTip = false
 
 Vue.component('tree-table', TreeTable)
+Vue.use(VueQuillEditor)
+
+Vue.filter('dateFormat', function (originVal) {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
